22019b28c3ee21ba616ae1c2f2b38b757ce8dfd3
traviezo <perseverancia@hotmail.com>
2017-02-24 15:00:18 -0500

    Update test.cpp
    Added 3 more tests to test the PidController constructor for invalid arguments.

--- test/test.cpp
+++ test/test.cpp
@@ -12,3 +12,18 @@ TEST(PidController, computePID2)
     PidController pidCtrl(0.5,2.0,0.1,0.1);
     EXPECT_EQ(25.5,pidCtrl.computePID(25.0,10.0));
 }
+
+TEST(PidConstructor, InvalidNegativeGainInputs)
+{
+    EXPECT_THROW(PidController(-22.0,-0.1,-5.7,1.0), std::domain_error);
+}
+
+TEST(PidConstructor, InvalidNegativeTimeArgument)
+{
+    EXPECT_THROW(PidController(18.0,7.3,1.5,-1.0), std::domain_error);
+}
+
+TEST(PidConstructor, InvalidZeroTimeArgument)
+{
+    EXPECT_THROW(PidController(34.0,3.6,2.8,0), std::runtime_error);
+}
